ad9c11d01d91d48161a3bacbe706030f8b114492
fangmingxing <1500152337@qq.com>
2019-07-28 15:30:21 +0800

    第一次提交

---
+++ src/main/java/com/star/service/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.star.service.impl;
+
+import com.star.service.UserService;
+
+public class UserServiceImpl implements UserService{
+
+}
---
+++ src/main/java/com/star/web/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.star.web.controller;
+
+public class UserController {
+
+}
